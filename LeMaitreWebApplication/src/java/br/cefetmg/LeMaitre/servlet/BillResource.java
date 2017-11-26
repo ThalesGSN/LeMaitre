819d83c035490327fb00e3913a954185217467c9
Unknown <thalesgsn@gmail.com>
2017-11-25 23:36:57 -0200

    Changes the way that bill is returned

--- LeMaitreWebApplication/src/java/br/cefetmg/LeMaitre/servlet/BillResource.java
+++ LeMaitreWebApplication/src/java/br/cefetmg/LeMaitre/servlet/BillResource.java
@@ -6,20 +6,30 @@
 package br.cefetmg.LeMaitre.servlet;
 
 import br.cefetmg.LeMaitre.model.dao.BillDAOImpl;
+import br.cefetmg.LeMaitre.model.dao.ItemDAOImpl;
+import br.cefetmg.LeMaitre.model.dao.ItemImageDAOImpl;
 import br.cefetmg.LeMaitre.model.dao.OrderDAOImpl;
+import br.cefetmg.LeMaitre.model.dao.SubcategoryDAOImpl;
 import br.cefetmg.LeMaitre.model.domain.Bill;
+import br.cefetmg.LeMaitre.model.domain.Image;
 import br.cefetmg.LeMaitre.model.domain.Item;
 import br.cefetmg.LeMaitre.model.domain.Order;
 import br.cefetmg.LeMaitre.model.exception.PersistenceException;
 import br.cefetmg.LeMaitre.model.service.BillManagement;
 import br.cefetmg.LeMaitre.model.service.BillManagementImpl;
+import br.cefetmg.LeMaitre.model.service.CategoryManagement;
+import br.cefetmg.LeMaitre.model.service.ItemImageManagement;
+import br.cefetmg.LeMaitre.model.service.ItemImageManagementImpl;
+import br.cefetmg.LeMaitre.model.service.ItemManagement;
+import br.cefetmg.LeMaitre.model.service.ItemManagementImpl;
 import br.cefetmg.LeMaitre.model.service.OrderManagement;
 import br.cefetmg.LeMaitre.model.service.OrderManagementImpl;
+import br.cefetmg.LeMaitre.model.service.SubcategoryManagement;
+import br.cefetmg.LeMaitre.model.service.SubcategoryManagementImpl;
 import br.cefetmg.LeMaitre.util.Result;
 import com.google.gson.Gson;
-import com.google.gson.internal.bind.JsonTreeWriter;
-import com.google.gson.stream.JsonWriter;
-import java.io.OutputStreamWriter;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.ws.rs.DELETE;
@@ -40,9 +50,11 @@ public class BillResource {
 
     private BillManagement billManagement;
     private OrderManagement orderManagement;
+    private ItemManagement itemManagement;
+    private ItemImageManagement itemImageManagement;
+    
     private Gson gson;
     private Result result;
-    private JsonWriter jWriter;
     
     /**
      * Creates a new instance of BillResource
@@ -60,21 +72,36 @@ public class BillResource {
     public String getBill(@PathParam("id") String id) {
         try {
             result = new Result();
+            gson = new Gson();
+            
             billManagement = new BillManagementImpl(BillDAOImpl.getInstance());
             orderManagement = new OrderManagementImpl(OrderDAOImpl.getInstance());
-            
-            gson = new Gson();
+            itemManagement = new ItemManagementImpl(ItemDAOImpl.getInstance());
+            itemImageManagement = new ItemImageManagementImpl(ItemImageDAOImpl.getInstance());
             
             Bill bill = billManagement.getBillByID(id);
             
             if (bill.getCodToken() == null) {
                 result.setStatusDOESNOTEXIST();
-
-            }
-            else {
-                BillOrdersAdapter billItemsAdapter;
-                billItemsAdapter = new BillOrdersAdapter(bill, orderManagement.getOrdersByToken(id));
-                result.setContent(billItemsAdapter);
+            } else {
+                
+                //Build the Complete Bill
+                List<OrderItemAdapter> ordersItemAdapter = new ArrayList();
+                for(Order order: orderManagement.getOrdersByToken(id)){
+                    
+                    Item item = itemManagement.getItemByID(order.getCodItem());
+                    List<Image> images = itemImageManagement.getImagesByItemID(order.getCodItem());
+                    ItemImageAdapter itemImageAdapter = new ItemImageAdapter(item, images);
+                    
+                    
+                    OrderItemAdapter orderItemAdapter = new OrderItemAdapter(order, itemImageAdapter);
+                    ordersItemAdapter.add(orderItemAdapter);
+                }
+                
+                BillOrdersAdapter billOrdersAdapter = 
+                        new BillOrdersAdapter(bill, ordersItemAdapter);
+               
+                result.setContent(billOrdersAdapter);
                 
                 result.setStatusOK();
             }
@@ -142,6 +169,7 @@ public class BillResource {
     
 }
 
+// <editor-fold defaultstate="collapsed" desc="Bill Adapters Classes. Click on the +">
 
 class BillOrdersAdapter{
     private String codToken;
@@ -150,24 +178,24 @@ class BillOrdersAdapter{
 
     private char idtStatus;
     
-    private List<Order> orders;
+    private List<OrderItemAdapter> orders;
 
     public BillOrdersAdapter() {}
 
-    public BillOrdersAdapter(String codToken, Date datUse, char idtStatus, List<Order> orders) {
+    public BillOrdersAdapter(String codToken, Date datUse, char idtStatus, List<OrderItemAdapter> orders) {
         this.codToken = codToken;
         this.datUse = datUse;
         this.idtStatus = idtStatus;
         this.orders = orders;
     }
     
-    public BillOrdersAdapter(Bill bill, List<Order> orders){
+    public BillOrdersAdapter(Bill bill, List<OrderItemAdapter> orders){
         this.codToken = bill.getCodToken();
         this.datUse = bill.getDatUse();
         this.idtStatus = bill.getIdtStatus();
         this.orders = orders;
-        
     }
+
 
     public String getCodToken() {
         return codToken;
@@ -193,13 +221,196 @@ class BillOrdersAdapter{
         this.idtStatus = idtStatus;
     }
 
-    public List<Order> getItems() {
+    public List<OrderItemAdapter> getItems() {
         return orders;
     }
 
-    public void setItems(List<Order> orders) {
+    public void setItems(List<OrderItemAdapter> orders) {
         this.orders = orders;
+    }
+    
+}
+
+class OrderItemAdapter{
+    private String codToken;
+    
+    private Timestamp datOrder;
+    
+    private Integer codItem;
+
+    private char idtStatus;
+
+    private double vlrPrice;
+
+    private int qtdItem;
+
+    private ItemImageAdapter item;
+
+    public OrderItemAdapter() {
+    }
+
+    public OrderItemAdapter(String codToken, Timestamp datOrder, Integer codItem,
+            char idtStatus, double vlrPrice, Integer qtdItem, ItemImageAdapter item) {
+        this.codToken = codToken;
+        this.datOrder = datOrder;
+        this.codItem = codItem;
+        this.idtStatus = idtStatus;
+        this.vlrPrice = vlrPrice;
+        this.qtdItem = qtdItem;
+        this.item = item;
+    }
+    
+    public OrderItemAdapter(Order order, ItemImageAdapter item) {
+        this.codToken = order.getCodToken();
+        this.datOrder = order.getDatOrder();
+        this.codItem = order.getCodItem();
+        this.idtStatus = order.getIdtStatus();
+        this.vlrPrice = order.getVlrPrice();
+        this.qtdItem = order.getQtdItem();
+        this.item = item;
+    }
+
+    public String getCodToken() {
+        return codToken;
+    }
+
+    public void setCodToken(String codToken) {
+        this.codToken = codToken;
+    }
+
+    public Timestamp getDatOrder() {
+        return datOrder;
+    }
+
+    public void setDatOrder(Timestamp datOrder) {
+        this.datOrder = datOrder;
+    }
+
+    public Integer getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(Integer codItem) {
+        this.codItem = codItem;
+    }
+
+    public char getIdtStatus() {
+        return idtStatus;
+    }
+
+    public void setIdtStatus(char idtStatus) {
+        this.idtStatus = idtStatus;
+    }
+
+    public double getVlrPrice() {
+        return vlrPrice;
+    }
+
+    public void setVlrPrice(double vlrPrice) {
+        this.vlrPrice = vlrPrice;
+    }
+
+    public Integer getQtdItem() {
+        return qtdItem;
+    }
+
+    public void setQtdItem(Integer qtdItem) {
+        this.qtdItem = qtdItem;
+    }
+
+    public ItemImageAdapter getImages() {
+        return item;
+    }
+
+    public void setImages(ItemImageAdapter item) {
+        this.item = item;
     }
     
     
 }
+
+class ItemImageAdapter{
+    private Integer codItem;
+
+    private Double vlrPrice;
+
+    private String nomItem;
+
+    private String desItem;
+
+    private boolean isAvaliable;
+    
+    private List<Image> images;
+
+    
+    public ItemImageAdapter() { }
+    
+    public ItemImageAdapter(Integer codItem, Double vlrPrice, String nomItem, 
+            String desItem, boolean isAvaliable, String seqCategory, String seqSubcategory, List<Image> images) {
+        this.codItem = codItem;
+        this.vlrPrice = vlrPrice;
+        this.nomItem = nomItem;
+        this.desItem = desItem;
+        this.isAvaliable = isAvaliable;
+        this.images = images;
+    }
+    
+    public ItemImageAdapter(Item item, List<Image> images){
+        this.codItem = item.getCodItem();
+        this.vlrPrice = item.getVlrPrice();
+        this.nomItem = item.getNomItem();
+        this.desItem = item.getDesItem();
+        this.isAvaliable = item.isAvaliable();
+        this.images = images;
+    }
+
+    public Integer getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(Integer codItem) {
+        this.codItem = codItem;
+    }
+
+    public Double getVlrPrice() {
+        return vlrPrice;
+    }
+
+    public void setVlrPrice(Double vlrPrice) {
+        this.vlrPrice = vlrPrice;
+    }
+
+    public String getNomItem() {
+        return nomItem;
+    }
+
+    public void setNomItem(String nomItem) {
+        this.nomItem = nomItem;
+    }
+
+    public String getDesItem() {
+        return desItem;
+    }
+
+    public void setDesItem(String desItem) {
+        this.desItem = desItem;
+    }
+
+    public boolean isIsAvaliable() {
+        return isAvaliable;
+    }
+
+    public void setIsAvaliable(boolean isAvaliable) {
+        this.isAvaliable = isAvaliable;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+}
+
+// </editor-fold>
